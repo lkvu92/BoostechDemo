820edb1fce53003c414aa9bb696c13df8f188e45
Long Le <lethaitongg@gmail.com>
2024-06-20 18:02:59 +0700

    check pvalue exist before inserting

--- src/main/java/com/boostech/demo/repository/PValueRepository.java
+++ src/main/java/com/boostech/demo/repository/PValueRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.boostech.demo.entity.PValue;
 
@@ -12,6 +13,9 @@ import com.boostech.demo.entity.PValue;
 public interface PValueRepository extends JpaRepository<PValue, UUID> {
 //	Optional<PValue> findByValueId_ProductId(UUID productId);
 //	Optional<PValue> findByValueId_AttributeId(UUID attributeId);
+	
+	boolean existsByProductIdAndAttributeId(@Param(value = "productId") UUID productId, @Param(value = "attributeId") UUID attributeId);
+	Optional<PValue> findByProductIdAndAttributeId(UUID productId, UUID attributeId);
 	Optional<PValue> findByProductIdAndAttributeIdAndDeletedAtIsNull(UUID productId, UUID attributeId);
 	List<PValue> findAllByAttributeIdIn(List<UUID> attributeIdList);
 	List<PValue> findAllByProductIdIn(List<UUID> productIdList);
