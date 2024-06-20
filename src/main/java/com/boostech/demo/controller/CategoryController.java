e8ad4294f6696cd88f6fe7d51543f1de10535592
Tran Vinh Phat <phattran1023@gmail.com>
2024-06-20 09:15:00 +0700

    CustomeResponse for Restful API

--- src/main/java/com/boostech/demo/controller/CategoryController.java
+++ src/main/java/com/boostech/demo/controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.boostech.demo.controller;
 import com.boostech.demo.dto.CategoryDTO;
 import com.boostech.demo.entity.Category;
 import com.boostech.demo.service.CategoryService;
+import com.boostech.demo.util.CustomResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,47 +21,105 @@ public class CategoryController {
     private CategoryService categoryService;
 
     @GetMapping
-    public List<Category> getAllCategories() {
-        return categoryService.getAllCategories();
+    public ResponseEntity<CustomResponse<List<Category>>> getAllCategories() {
+        List<Category> categories = categoryService.getAllCategories();
+        CustomResponse<List<Category>> response = new CustomResponse<>("Success", HttpStatus.OK.value(), categories);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     /**
      * Get category by id
+     *
      * @param id category id
      * @return category
      */
     @GetMapping("/{id}")
-    public Optional<Category> getCategoryById(@PathVariable UUID id) {
-        return categoryService.getCategoryById(id);
+    public ResponseEntity<CustomResponse<Category>> getCategoryById(@PathVariable UUID id) {
+        Optional<Category> categoryOpt = categoryService.getCategoryById(id);
+        if (categoryOpt.isPresent()) {
+            CustomResponse<Category> response = new CustomResponse<>
+                    ("Success", HttpStatus.OK.value(), categoryOpt.get());
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }
+        CustomResponse<Category> response = new CustomResponse<>
+                ("Category not found", HttpStatus.NOT_FOUND.value(), null);
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
+    /**
+     * Create a new category
+     *
+     * @param categoryDTO category data
+     * @return category
+     */
     @PostMapping("/create")
-    public ResponseEntity<Category> createCategory(@RequestBody CategoryDTO categoryDTO) {
-
+    public ResponseEntity<CustomResponse<Category>> createCategory(@RequestBody CategoryDTO categoryDTO) {
         Category category = categoryService.createCategory(categoryDTO);
-        return new ResponseEntity<>(category, HttpStatus.CREATED);
+        CustomResponse<Category> response = new CustomResponse<>("Success", HttpStatus.CREATED.value(), category);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
+
+    /**
+     * Update a category
+     *
+     * @param id category id
+     * @param categoryDTO category data
+     * @return category
+     */
 
     @PutMapping("/{id}")
-    public ResponseEntity<Category> updateCategory(@PathVariable UUID id, @RequestBody CategoryDTO categoryDTO) {
-
+    public ResponseEntity<CustomResponse<Category>> updateCategory(
+            @PathVariable UUID id,
+            @RequestBody CategoryDTO categoryDTO) {
         Category category = categoryService.updateCategory(id, categoryDTO);
-        return new ResponseEntity<>(category, HttpStatus.OK);
+        CustomResponse<Category> response = new CustomResponse<>
+                ("Success", HttpStatus.OK.value(), category);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+
+    /**
+     * Delete a category
+     *
+     * @param id category id
+     * @return success message
+     */
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteCategory(@PathVariable UUID id) {
+    public ResponseEntity<CustomResponse<String>> deleteCategory(@PathVariable UUID id) {
         categoryService.deleteCategory(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        CustomResponse<String> response = new CustomResponse<>
+                ("Category deleted successfully", HttpStatus.OK.value(), null);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    /**
+     * Add attributes to category
+     *
+     * @param id category id
+     * @param categoryDTO category data
+     * @return category
+     */
     @PostMapping("/{id}/addAttributes")
-    public Category addAttributesToCategory(@PathVariable UUID id, @RequestBody CategoryDTO categoryDTO) {
-        return categoryService.addAttributesToCategory(id, categoryDTO.getAttributeIds());
+    public ResponseEntity<CustomResponse<Category>> addAttributesToCategory(@PathVariable UUID id, @RequestBody CategoryDTO categoryDTO) {
+        Category category = categoryService.addAttributesToCategory(id, categoryDTO.getAttributeIds());
+        CustomResponse<Category> response = new CustomResponse<>("Success", HttpStatus.OK.value(), category);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    /**
+     * Remove attributes from category
+     *
+     * @param id category id
+     * @param categoryDTO category data
+     * @return category
+     */
     @PostMapping("/{id}/removeAttributes")
-    public Category removeAttributesFromCategory(@PathVariable UUID id, @RequestBody CategoryDTO categoryDTO) {
-        return categoryService.removeAttributesFromCategory(id, categoryDTO.getAttributeIds());
+    public ResponseEntity<CustomResponse<Category>> removeAttributesFromCategory(
+            @PathVariable UUID id,
+            @RequestBody CategoryDTO categoryDTO) {
+        Category category = categoryService.removeAttributesFromCategory(id, categoryDTO.getAttributeIds());
+        CustomResponse<Category> response = new CustomResponse<>
+                ("Success", HttpStatus.OK.value(), category);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/boostech/demo/util/CustomResponse.java
@@ -0,0 +1,14 @@
+package com.boostech.demo.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomResponse<T> {
+    private String message;
+    private int status;
+    private T data;
+}
